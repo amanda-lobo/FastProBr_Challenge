27d4b4e57bc6845bd1791cb6be647cc48e74b5e0
amanda-lobo <amandalobo.ag@gmail.com>
2023-12-15 13:13:15 -0300

    Correção do Token

--- service/userService.js
+++ service/userService.js
@@ -40,7 +40,7 @@ exports.loginUser = async (req, res) => {
 
         const token = jwt.sign(
             { userId: user.id },
-            'minhaChaveSecreta',
+            process.env.SECRET_KEY,
             { expiresIn: '1h' } 
         );
 
